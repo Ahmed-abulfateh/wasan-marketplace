d727546f61edcac9465b4b9f38c759ddf63f6632
Ahmed-abulfateh <ahmed-bh91@live.com>
2026-05-10 09:44:40 +0300

    Use provided ChatGPT image in header

--- src/components/MarketplaceLayout.tsx
+++ src/components/MarketplaceLayout.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link, NavLink, Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom'
-import chatgptBadgeSrc from '../assets/chatgpt-badge.svg'
+import chatgptBadgeSrc from '../assets/chatgpt-logo.png'
 import logoSrc from '../assets/logo-wasan.svg'
 import { useLanguage } from '../context/LanguageContext'
 import { useMarketplace } from '../context/MarketplaceContext'
